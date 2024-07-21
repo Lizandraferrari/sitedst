1014dbdb6357bda8d79bf5f3e58369e03380d274
Lizandra de Jesus Ferrari <lizandra.ferrari@hotmail.com>
2024-07-20 22:56:28 -0300

    CSS Responsivo e imagem NAV

--- dist/src/Nav.jsx
+++ dist/src/Nav.jsx
@@ -1,10 +1,13 @@
 import 'bootstrap/js/dist/dropdown';
+import logo from'./img/png.png';
+
 
 export default function Nav() {
   return (
     <nav class="navbar navbar-expand-lg nav_estilo">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="../img/png.png" width="10%" /></a>
+        <a class="navbar-brand" href="#">
+          <img src={logo} width="10%" /></a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
